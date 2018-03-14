7b11f7e56a02b77dd427f37fd6991ec2de7cb5e4
jr-selphius <sanrafja@hotmail.com>
2018-03-14 22:43:26 +0100

    First commit.

---
+++ app/src/main/java/com/marvel/jr/supers/CustomApplication.kt
@@ -0,0 +1,13 @@
+package com.marvel.jr.supers
+
+import android.app.Application
+
+class CustomApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        //things
+
+    }
+}
---
+++ app/src/main/java/com/marvel/jr/supers/ui/HeroesListActivity.kt
@@ -0,0 +1,13 @@
+package com.marvel.jr.supers.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.marvel.jr.supers.R
+
+class HeroesListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_heroes_list)
+    }
+}
